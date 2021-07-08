2b49db6256e3659576232ecab02f430c6661368e
Overfirst <overfirst@inbox.ru>
2021-07-08 13:33:18 +0300

    Добавил shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, CommonModule],
+  exports: [BrowserModule, CommonModule]
+})
+export class SharedModule {}

7a7e6685ad2d369b9139829e1baa2c025bc87a8d
Overfirst <overfirst@inbox.ru>
2021-07-13 15:48:02 +0300

    Обновление набора данных

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,38 @@ export class AppComponent {
   public tasks: GanttTask[] = [
     {
       name: 'Create gantt component',
-      startDate: new Date(2014, 5, 2, 5, 0),
-      endDate: new Date(2014, 6, 12, 4, 0),
-      readyPercent: 15
+      startDate: new Date(2021, 5, 1, 2, 0),
+      endDate: new Date(2021, 5, 25, 12, 0),
+      readyPercent: 66,
+      color: '#ff6e6e'
     },
     {
-      name: 'RATATATATATATATATA',
-      startDate: new Date(2014, 5, 2, 4, 0),
-      endDate: new Date(2014, 6, 12, 4, 0),
-      readyPercent: 93
+      name: 'Fix angular-grid bugs',
+      startDate: new Date(2021, 5, 1, 12),
+      endDate: new Date(2021, 5, 9, 13),
+      readyPercent: 71,
+      color: '#5a63cf'
     },
-    // {
-    //   name: 'Fix angular-grid bugs',
-    //   startDate: new Date(2021, 6, 7),
-    //   endDate: new Date(2021, 7, 14),
-    //   readyPercent: 77
-    // },
-    // {
-    //   name: 'Survive at work',
-    //   startDate: new Date(2021, 5, 23),
-    //   endDate: new Date(2021, 9, 25),
-    //   readyPercent: 1
-    // }
+    {
+      name: 'Survive at work',
+      startDate: new Date(2021, 5, 2, 3, 20),
+      endDate: new Date(2021, 6, 25),
+      readyPercent: 60,
+      color: '#39e30f'
+    },
+    {
+      name: 'Complete the internship',
+      startDate: new Date(2021, 4, 31, 6, 33),
+      endDate: new Date(2021, 5, 13, 12, 30),
+      readyPercent: 75,
+      color: '#8c22f6'
+    },
+    {
+      name: 'Wait teamlead from the day off',
+      startDate: new Date(2021, 5, 2, 16, 45),
+      endDate: new Date(2021, 5, 16, 12, 30),
+      readyPercent: 61,
+      color: '#05e9f4'
+    }
   ];
 }

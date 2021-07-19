0c0f99d74b32d3e71a558215eb6eb0cc9c288561
Overfirst <overfirst@inbox.ru>
2021-07-19 12:13:42 +0300

    Вернул необходимый геттер

--- src/app/shared/components/gantt/gantt-dependencies/gantt-dependencies.component.ts
+++ src/app/shared/components/gantt/gantt-dependencies/gantt-dependencies.component.ts
@@ -14,14 +14,21 @@ export class GanttDependenciesComponent {
   @Input() public width = 0;
   @Input() public height = 0;
 
+  private _data: GanttDependenciesData;
+
   public lines: GanttLine[] = [];
 
   @Input() public set data(data: GanttDependenciesData) {
     if (this.data && data.tasksInfo === this.data.tasksInfo && data.dependencies === this.data.dependencies && data.period === this.data.period) {
       return;
     }
-    
+
+    this._data = data;
     console.log('dependencies data set:', data);
     this.lines = this.service.computeDependencies(this.data);
+  }
+
+  public get data() {
+    return this._data;
   }
 }

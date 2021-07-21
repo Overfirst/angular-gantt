572e9ec538f3be59c2c0d48309810c178d37b9f1
Overfirst <overfirst@inbox.ru>
2021-07-21 10:00:21 +0300

    Фикс битого таймлайна

--- src/app/shared/components/gantt/gantt-timeline/gantt-timeline.component.ts
+++ src/app/shared/components/gantt/gantt-timeline/gantt-timeline.component.ts
@@ -185,11 +185,11 @@ export class GanttTimelineComponent implements AfterViewInit, OnDestroy {
   }
 
 
-  public getTaskProgressData(idx: number): TaskProgressInput {
-    this.showDependencies = idx === this.tasks.length - 1;
+  public getTaskProgressData(taskRow: GanttTaskRow): TaskProgressInput {
+    // this.showDependencies = idx === this.tasks.length - 1;
 
     return {
-      taskInfo: { task: this.tasks[idx], rowID: idx + 1 },
+      taskInfo: { task: taskRow.task, rowID: -1 },
       period: this.period,
       minDate: this.periodParts[0].detail[0]
     };
